8ba5b429e1b41d70353a8ea6a708a5cba770ba2d
Raj Gohil <raj.famous007@gmail.com>
2023-10-04 09:56:23 +0530

    Made the relavent changes to prevent the invalid pass of the connection and model changes

---
+++ src/Model/Index.ts
@@ -0,0 +1,17 @@
+module.exports = () => {
+  const Mongoose = require("mongoose");
+
+  // Connection to mongoDB.
+  Mongoose.connect(
+    process.env.MONGOOES_PATH!,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    },
+    (err: any) =>
+      err
+        ? console.log("Enable to connect to the database due to:", err)
+        : console.log("MongoDB connection successful!")
+  );
+};
