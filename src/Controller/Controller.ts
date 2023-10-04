e02f67f61f6d599ed4996043199b58253363a76b
Khyati Ratandhayara <kthakkar9426@gmail.com>
2023-10-04 23:32:13 +0530

    added route for edit profile

--- src/Controller/Controller.ts
+++ src/Controller/Controller.ts
@@ -1,5 +1,8 @@
-import product_model from '../Modal/Product_info';
+import product_model from '../Model/Product_info';
+import seller_model from '../Model/Seller_info';
 const product = product_model();
+const seller = seller_model();
+
 
 exports.NotFound = (req: any, res: any) => res.send({ message: 'invalid URL!' });
 
@@ -53,3 +56,36 @@ exports.showProfit = async (req: any, res: any) => {
     ])
     profit.length ? res.send({ Message: "Profit of product ", profit }) : res.send({ message: "No Product to display" });
 };
+
+exports.editProduct = async (req, res) => {
+    try {
+      const productId = req.params.id;
+      const updates = {
+        name: req.body.name,
+        price: req.body.price,
+        quantity: req.body.quantity
+      };
+  
+      // Find and update the product by ID
+      const updatedProduct = await product.findByIdAndUpdate(productId, updates, {
+        new: true, // Return the updated product
+        runValidators: true // Run validators to ensure data validity
+      });
+  
+      if (!updatedProduct) {
+        return res.status(404).send({ message: 'Product not found' });
+      }
+  
+      res.send({ message: 'Product updated successfully', updatedProduct });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({ message: 'Internal server error' });
+    }
+  };
+
+  
+
+  
+  
+  
+  
