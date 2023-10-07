7217ab44a3d5747966f2bdc7a5dc8cafb680e145
Rahul Rathod <rahulrathod2736@gmail.com>
2023-10-07 09:24:57 +0530

    fix: fixed comments, types and resolved issues

--- src/Controller/Seller_Controller.ts
+++ src/Controller/Seller_Controller.ts
@@ -1,55 +1,66 @@
-import product_model from '../Modal/Product_info';
+import { Request, Response } from "express";
+import product_model from "../Model/Product_info";
+import SellerInfo from "../Model/Seller_info";
+
 const product = product_model();
+const seller = SellerInfo();
 
-exports.NotFound = (req: any, res: any) => res.send({ message: 'invalid URL!' });
+exports.NotFound = (req: Request, res: Response) =>
+  res.status(404).send({ message: "invalid URL!" });
 
-exports.insertProduct = async (req: any, res: any) => {
+exports.insertProduct = async (req: Request, res: Response) => {
+  const data = new product({
+    name: req.body.name,
+    price: req.body.price,
+    quantity: req.body.quantity,
+  });
 
-    const data = new product({
-        name: req.body.name,
-        price: req.body.price,
-        quantity: req.body.quantity
-    });
+  const result = await data.save();
 
-    const result = await data.save();
-
-    res.send({ message: "new Product has been added", result });
-}
-
-exports.listProduct = async (req: any, res: any) => {
-
-    const data = await product.find();
-    data.length ? res.send({ Message: "Available Product data", data }) : res.send({ message: "No Product data to display" });
-}
-
-exports.removeProduct = async (req: any, res: any) => {
-
-    const product_id: string = req.params.id;
-    const deleted_data = await product.findByIdAndDelete(product_id);
-
-    const msg_string: any = deleted_data
-        ? { message: "data delete sucessfull!", Deleted_data: deleted_data }
-        : { message: "No data to Delete" };
-
-    res.send(msg_string);
+  res.send({ message: "new Product has been added", result });
 };
 
-exports.removeSeller = async (req: any, res: any) => {
-
-    const seller_id: string = req.params.id;
-    const deleted_data = await seller.findByIdAndDelete(seller_id);
-
-    const msg_string: any = deleted_data
-        ? { message: "data delete sucessfull!", Deleted_data: deleted_data }
-        : { message: "No data to Delete" };
-
-    res.send(msg_string);
+exports.listProduct = async (req: Request, res: Response) => {
+  const data = await product.find();
+  data.length
+    ? res.send({ Message: "Available Product data", data })
+    : res.send({ message: "No Product data to display" });
 };
 
-exports.showProfit = async (req: any, res: any) => {
+exports.removeProduct = async (req: Request, res: Response) => {
+  const product_id: string = req.params.id;
+  const deleted_data = await product.findByIdAndDelete(product_id);
 
-    const profit: any = await product.aggregate([
-        { $project: { name: 1, _id: 0, 'total_profit': { $multiply: ['$price', '$quantity'] } } }
-    ])
-    profit.length ? res.send({ Message: "Profit of product ", profit }) : res.send({ message: "No Product to display" });
+  const msg_string: any = deleted_data
+    ? { message: "data delete sucessfull!", Deleted_data: deleted_data }
+    : { message: "No data to Delete" };
+
+  res.send(msg_string);
+};
+
+exports.removeSeller = async (req: Request, res: Response) => {
+  const seller_id: string = req.params.id;
+  const deleted_data = await seller.findByIdAndDelete(seller_id);
+
+  const msg_string: any = deleted_data
+    ? { message: "data delete sucessfull!", Deleted_data: deleted_data }
+    : { message: "No data to Delete" };
+
+  res.send(msg_string);
+};
+
+exports.showProfit = async (req: Request, res: Response) => {
+  const profit: any = await product.aggregate([
+    {
+      $project: {
+        name: 1,
+        _id: 0,
+        total_profit: { $multiply: ["$price", "$quantity"] },
+      },
+    },
+  ]);
+  
+  profit.length
+    ? res.send({ Message: "Profit of product ", profit })
+    : res.send({ message: "No Product to display" });
 };
--- src/Router/Router.ts
+++ src/Router/Router.ts
@@ -1,31 +1,32 @@
 module.exports = (app: any) => {
-
-    const Middleware = require('../Middleware/Validator');
-    const Controller = require('../Controller/Controller');
-    const Seller_Controller = require('../Controller/Seller_Controller');
-
-    //insert Product
-    app.post('/insert', Middleware.Validate, Controller.insertProduct);
-
-    //to display product
-    app.get('/display', Controller.listProduct);
-
-    //to get product profit
-    app.get('/display_profit', Controller.showProfit);
-
-    //to remove Product
-    app.delete('/removeProduct/:id', Controller.removeProduct);
-
-    //to remove Product
-    app.delete('/removeSeller/:sellerID', Controller.removeSeller);
-    // edit product
-    app.put('/removeSeller/:sellerID', Controller.removeSeller);
-
-    app.get('/calculateTotalCost', Controller.calculateTotalCost);
-
-    app.get('/countCartItems', Controller.countCartItems);
-
-
-    //if invalid url passed
-    app.get('*', Controller.NotFound);
-}
+    const Middleware = require("../Middleware/Validator");
+    const Controller = require("../Controller/Controller");
+  
+    // Insert Product with validation
+    app.post("/insert", Middleware.Validate, Controller.insertProduct);
+  
+    // To display list of products
+    app.get("/display", Controller.listProduct);
+  
+    // To get product profit
+    app.get("/display_profit", Controller.showProfit);
+  
+    // To remove product by id
+    app.delete("/removeProduct/:id", Controller.removeProduct);
+  
+    // To remove seller by seller id
+    app.delete("/removeSeller/:sellerID", Controller.removeSeller);
+  
+    // Edit product
+    app.put("/removeSeller/:sellerID", Controller.removeSeller);
+  
+    // To calculate total cost
+    app.get("/calculateTotalCost", Controller.calculateTotalCost);
+  
+    // To count cart items
+    app.get("/countCartItems", Controller.countCartItems);
+  
+    // If any other url passed that's not listed above, will return not found
+    app.get("*", Controller.NotFound);
+  };
+  
