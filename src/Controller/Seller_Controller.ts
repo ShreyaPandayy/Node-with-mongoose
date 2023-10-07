d48f1b15911b0f07e0b88836199a177d18c854e8
Rahul Rathod <rahulrathod2736@gmail.com>
2023-10-07 09:47:18 +0530

    fix: fixed response structure

--- src/Controller/Seller_Controller.ts
+++ src/Controller/Seller_Controller.ts
@@ -22,31 +22,40 @@ exports.insertProduct = async (req: Request, res: Response) => {
 
 exports.listProduct = async (req: Request, res: Response) => {
   const data = await product.find();
-  data.length
-    ? res.send({ Message: "Available Product data", data })
-    : res.send({ message: "No Product data to display" });
+
+  if (data.length) {
+    return res.status(200).send({ message: "Available Product data", data });
+  }
+
+  return res.status(200).send({ message: "No Product data to display" });
 };
 
 exports.removeProduct = async (req: Request, res: Response) => {
   const product_id: string = req.params.id;
-  const deleted_data = await product.findByIdAndDelete(product_id);
+  const deletedData = await product.findByIdAndDelete(product_id);
 
-  const msg_string: any = deleted_data
-    ? { message: "data delete sucessfull!", Deleted_data: deleted_data }
-    : { message: "No data to Delete" };
+  if (deletedData) {
+    return res.status(200).send({
+      message: "Product delete sucessfully!",
+      deletedData,
+    });
+  }
 
-  res.send(msg_string);
+  return res.status(200).send({ message: "No data to Delete" });
 };
 
 exports.removeSeller = async (req: Request, res: Response) => {
   const seller_id: string = req.params.id;
-  const deleted_data = await seller.findByIdAndDelete(seller_id);
+  const deletedData = await seller.findByIdAndDelete(seller_id);
 
-  const msg_string: any = deleted_data
-    ? { message: "data delete sucessfull!", Deleted_data: deleted_data }
-    : { message: "No data to Delete" };
+  if (deletedData) {
+    return res.status(200).send({
+      message: "Seller deleted sucessfully!",
+      deletedData,
+    });
+  }
 
-  res.send(msg_string);
+  return res.status(200).send({ message: "No data to Delete" });
 };
 
 exports.showProfit = async (req: Request, res: Response) => {
@@ -59,8 +68,10 @@ exports.showProfit = async (req: Request, res: Response) => {
       },
     },
   ]);
-  
-  profit.length
-    ? res.send({ Message: "Profit of product ", profit })
-    : res.send({ message: "No Product to display" });
+
+  if (profit.length) {
+    return res.status(200).send({ message: "Profit of product ", profit });
+  }
+
+  return res.status(200).send({ message: "No Product to display" });
 };
--- src/Middleware/Validator.ts
+++ src/Middleware/Validator.ts
@@ -1,17 +1,6 @@
 import Joi from "joi";
 
 exports.Validate = (req: any, res: any, cb: any) => {
-  const ProductSchema: any = Joi.object({
-    name: Joi.string()
-      .pattern(new RegExp("^[a-zA-z]{3,50}$"))
-      .min(3)
-      .required(),
-    price: Joi.number().required(),
-    quantity: Joi.number().required(),
-    ratings: Joi.number().required(),
-    email: Joi.string().email().required(),
-  });
-
   const SellerSchema: any = Joi.object({
     name: Joi.string()
       .pattern(new RegExp("^[a-zA-z]{3,50}$"))
